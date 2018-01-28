e9834163c12e138989589bb3a8f2b75149b39720
Jayden Chan <Jayden.T.Chan@gmail.com>
2018-01-28 02:38:25 -0700

    Mathematicians got to this GUI

--- pyBraille.py
+++ pyBraille.py
@@ -25,34 +25,34 @@ class pyBraille:
         self.master = master
         master.title("pyBraille")
 
-        self.label = Label(master, text="Disconnected").grid(row=0,column=0)
-        self.label.pack()
+        self.label = Label(master, text="Disconnected")
+        self.label.grid(row=0,column=0)
         
-        self.port_box = Text(root, height = 1, width = 10).grid(row=0,column=0)
-        self.port_box.pack()
+        self.port_box = Text(root, height = 1, width = 10)
+        self.port_box.grid(row=1,column=0)
         self.port_box.insert(END, "COM5")
 
-        self.connect_button = Button(master, text="Connect", command=self.connect).grid(row=0,column=0)
-        self.connect_button.pack()
+        self.connect_button = Button(master, text="Connect", command=self.connect)
+        self.connect_button.grid(row=2,column=0)
         
-        self.send_button = Button(master, text="Send", state=DISABLED, command=self.send).grid(row=0,column=0)
-        self.send_button.pack()
+        self.send_button = Button(master, text="Send", state=DISABLED, command=self.send)
+        self.send_button.grid(row=3,column=0)
 
-        self.close_button = Button(master, text="Close", command=self.quitProg).grid(row=0,column=0)
-        self.close_button.pack()
+        self.close_button = Button(master, text="Close", command=self.quitProg)
+        self.close_button.grid(row=4,column=0)
 
-        self.labelDescription = Label(master, text = "Open a file to read to text", fg = "blue").grid(row=0,column=1)
-        self.labelDescription.pack(side=RIGHT)
+        self.labelDescription = Label(master, text = "Open a file to read to text", fg = "blue")
+        self.labelDescription.grid(row=1,column=1)
 
-        self.entryBox = Entry(master, textvariable = entryVar).grid(row=0,column=1)
-        self.entryBox.pack(side=RIGHT, fill=X)
+        self.entryBox = Entry(master, textvariable = entryVar)
+        self.entryBox.grid(row=2,column=1)
 
 
-        self.buttonOpen = Button(master, text = "Open", fg = "red", command=lambda: transcribeFile(os.path.basename(self.entryBox.get()))).grid(row=0,column=1)
-        self.buttonOpen.pack()
+        self.buttonOpen = Button(master, text = "Open", fg = "red", command=lambda: transcribeFile(os.path.basename(self.entryBox.get())))
+        self.buttonOpen.grid(row=3,column=1)
 
-        self.buttonFile = Button(master, text = "...", command = self.openDir).grid(row=0,column=1)
-        self.buttonFile.pack(side = RIGHT)
+        self.buttonFile = Button(master, text = "...", command = self.openDir)
+        self.buttonFile.grid(row=4,column=1)
 
     def connect(self):
         print("Greetings!")
