574a37078b41380d8cf5f479b95911ef42f6dc2c
Jayden Chan <Jayden.T.Chan@gmail.com>
2018-01-27 23:46:27 -0700

    A Commit

--- pyBraille.py
+++ pyBraille.py
@@ -1,7 +1,12 @@
 from tkinter import *
 import serialCom as sc
+import threading
 
 class pyBraille:
+
+    # === Variables
+    threads = []
+
     def __init__(self, master):
         self.master = master
         master.title("pyBraille")
@@ -15,8 +20,11 @@ class pyBraille:
 
         self.greet_button = Button(master, text="Connect", command=self.connect)
         self.greet_button.pack()
+        
+        self.send_button = Button(master, text="Send", state=DISABLED, command=self.self)
+        self.send_button.pack()
 
-        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button = Button(master, text="Close", command=self.quitProg)
         self.close_button.pack()
 
     def connect(self):
@@ -24,9 +32,18 @@ class pyBraille:
         arduino = self.port_box.get("1.0", END).rstrip("\n\r")
         if sc.connect(arduino):
             self.label['text'] = 'Connected to ' + arduino
+            self.send_button['state'] = 'normal'
         else:
             self.label['text'] = 'Connection Failed'
 
+    def send(self):
+        sc.sendBraille()
+        
+    def quitProg(self):
+        sys.exit(0)
+        
+
+            
 root = Tk()
 my_gui = pyBraille(root)
 root.mainloop()
--- serialCom.py
+++ serialCom.py
@@ -2,21 +2,38 @@
 import serial
 
 # ==== Variables
+ser = serial.Serial()
 
 def connect(port):
-    ser = serial.Serial(port, 9600)
+    ser.port = port
+    ser.baudrate = 9600
+    ser.open()
     
     try:
         serin = ser.read()
             
-        ser.write("1".encode())
+        ser.write("8".encode())
 
-        while ser.read() == '1':
-            ser.read()
-            
         return True
     except:
         return False
         
+def sendBraille():
+    #⠠⠓⠑⠇⠇⠕⠀⠠⠸⠺
+    list = ['6', '1-2-5', '1-5', '1-2-3', '1-2-3', '1-3-5', '', '6', '4-5-6', '2-4-5-6']
+    
+    for l in list:
+        parse = l.split('-')
+        print(parse)
+        parse.append('0')
+        
+        for p in parse:
+            ser.write(p.encode())
+            print(p)
+            ser.read()
+            
+    sys.exit(0)
+
 if __name__ == "__main__":
     connect("COM5")
+    sendBraille()
